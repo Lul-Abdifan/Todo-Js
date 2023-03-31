70d925e22c4606d13225bd350508492b4945b6aa
hope <tasanbannu@gmail.com>
2023-03-31 11:37:54 -0400

    Add checkbox toggling functionality

--- src/components/render.js
+++ src/components/render.js
@@ -1,30 +1,44 @@
-import { EditTask } from './crud/update.js';
-import { lists } from './variables.js';
-import { tasks } from '../index.js';
+import { EditTask } from "./crud/update.js";
+import { lists } from "./variables.js";
+import { tasks } from "../index.js";
+import { storeToLocal } from "./storage.js";
 
 const handleEdit = (index) => {
   EditTask(index);
 };
+const handleCheckbox = (event) => {
+  const checkbox = event.target;
+  const taskIndex = checkbox.dataset.index;
+  const task = tasks[taskIndex];
+  task.completed = checkbox.checked;
+  storeToLocal(tasks);
+  renderAll();
+};
+
 const renderAll = () => {
-  lists.innerHTML = '';
+  lists.innerHTML = "";
   tasks.forEach((task, index) => {
-    const div = document.createElement('div');
-    const hr = document.createElement('hr');
-    div.classList.add('task');
+    const div = document.createElement("div");
+    const hr = document.createElement("hr");
+    div.classList.add("task");
     div.innerHTML = `
-        <h2>
-          <input type="checkbox"/>
-          <h2 class="input-board"> ${task.description}</h2>
-         </h2>
-        <div> 
-        <i class="fa fa-pencil edit-icon" data-index="${index}"></i> 
-        <i class="fas fa-trash-alt delete-btn" data-index="${index}"></i>       
+      <h2>
+        <input type="checkbox" class="completed" data-index="${index}" ${
+      task.completed ? "checked" : ""
+    }>
+        <h2 class="input-board">${task.description}</h2>
+      </h2>
+      <div>
+        <i class="fa fa-pencil edit-icon" data-index="${index}"></i>
+        <i class="fas fa-trash-alt delete-btn" data-index="${index}"></i>
         <i class="fa-solid fa-ellipsis-vertical"></i>
-        </div>
-      `;
+      </div>
+    `;
+    const checkbox = div.querySelector(".completed");
+    checkbox.addEventListener("change", handleCheckbox);
     div
-      .querySelector('.edit-icon')
-      .addEventListener('click', () => handleEdit(index));
+      .querySelector(".edit-icon")
+      .addEventListener("click", () => handleEdit(index));
     lists.append(div);
     lists.append(hr);
   });
